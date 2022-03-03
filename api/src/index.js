b0534fae3e9dabfe283e25f791c2b2cd730473a2
fuxleep <MaddisonMatthewsWebDev@gmail.com>
2022-03-03 16:51:59 +0300

    GraphQL: getting all the notes && by id

--- api/src/index.js
+++ api/src/index.js
@@ -3,14 +3,28 @@ const { ApolloServer, gql } = require('apollo-server-express');
 
 const port = process.env.PORT || 4000;
 
+let notes = [
+  { id: '1', content: 'This is a note', author: 'Adam Scott' },
+  { id: '2', content: 'This is another note', author: 'Harlow Everly' },
+  { id: '3', content: 'Oh hey look, another note!', author: 'Riley Harrison' },
+];
+
 const typeDefs = gql`
   type Query {
-    hello: String
+    notes: [Note!]!
+    note(id: ID!): Note!
+  }
+  type Note {
+    id: ID!
+    content: String!
+    author: String!
   }
 `;
+
 const resolvers = {
   Query: {
-    hello: () => 'Hello World!',
+    notes: () => notes,
+    note: (_, args) => notes.find(note => note.id === args.id),
   },
 };
 
