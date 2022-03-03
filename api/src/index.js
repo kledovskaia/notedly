d6af5d019450f21f4374193656962bd3163b5d6c
fuxleep <MaddisonMatthewsWebDev@gmail.com>
2022-03-03 16:29:13 +0300

    GraphQL simple server

--- api/src/index.js
+++ api/src/index.js
@@ -1,2 +1,27 @@
-// index.js
-// This is the main entry point of our application
+const express = require('express');
+const { ApolloServer, gql } = require('apollo-server-express');
+
+const port = process.env.PORT || 4000;
+
+const typeDefs = gql`
+  type Query {
+    hello: String
+  }
+`;
+const resolvers = {
+  Query: {
+    hello: () => 'Hello World!',
+  },
+};
+
+const app = express();
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
+server.applyMiddleware({ app, path: '/api' });
+
+app.listen({ port }, () => {
+  console.log(
+    `GraphQL Server is running at http://localhost:${port}${server.graphqlPath}`
+  );
+});
