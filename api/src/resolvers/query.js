3408b1eeeb3c67ac0027c4fee96313cb9d233bbd
fuxleep <MaddisonMatthewsWebDev@gmail.com>
2022-03-04 13:01:06 +0300

    Updated: query me funtionality

--- api/src/resolvers/query.js
+++ api/src/resolvers/query.js
@@ -1,11 +1,14 @@
 const note = async (_, { id }, { models }) => await models.Note.findById(id);
 const notes = async (_, __, { models }) => await models.Note.find();
 
-const me = async (_, __, { user }) => user;
-const user = async (_, { id, username }, { models }) =>
-  await models.User.findOne({
+const me = async (_, __, { models, user }) => {
+  return await models.User.findById(user.id);
+};
+const user = async (_, { id, username }, { models }) => {
+  return await models.User.findOne({
     $or: [{ _id: id }, { username }],
   });
+};
 const users = async (_, __, { models }) => await models.User.find();
 
 module.exports = {
