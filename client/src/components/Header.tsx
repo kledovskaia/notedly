ba9a41f9a77de61faf0f0425f55c00d81854f2e7
fuxleep <MaddisonMatthewsWebDev@gmail.com>
2022-03-05 12:17:48 +0300

    Added: light/dark theme switch

---
+++ client/src/components/Header.tsx
@@ -0,0 +1,84 @@
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import Badge from '@mui/material/Badge';
+import MenuItem from '@mui/material/MenuItem';
+import Menu from '@mui/material/Menu';
+import NotificationsIcon from '@mui/icons-material/Notifications';
+import { Avatar } from '@mui/material';
+import { ThemeSwitch } from './ThemeSwitch';
+
+export default function Header() {
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+
+  const isMenuOpen = Boolean(anchorEl);
+
+  const handleProfileMenuOpen = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleMenuClose = () => {
+    setAnchorEl(null);
+  };
+
+  const menuId = 'account-menu';
+  const renderMenu = (
+    <Menu
+      anchorEl={anchorEl}
+      anchorOrigin={{
+        vertical: 'top',
+        horizontal: 'right',
+      }}
+      id={menuId}
+      keepMounted
+      transformOrigin={{
+        vertical: 'top',
+        horizontal: 'right',
+      }}
+      open={isMenuOpen}
+      onClose={handleMenuClose}
+    >
+      <MenuItem onClick={handleMenuClose}>Logout</MenuItem>
+    </Menu>
+  );
+
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar sx={{ boxShadow: 'none' }} position="static">
+        <Toolbar>
+          <Typography variant="h6" noWrap component="div">
+            Notedly
+          </Typography>
+          <Box sx={{ flexGrow: 1 }} />
+          <Box sx={{ display: { md: 'flex' }, alignItems: 'center' }}>
+            <ThemeSwitch />
+            <IconButton
+              size="large"
+              aria-label="show 17 new notifications"
+              color="inherit"
+            >
+              <Badge badgeContent={17} color="error">
+                <NotificationsIcon />
+              </Badge>
+            </IconButton>
+            <IconButton
+              size="large"
+              edge="end"
+              aria-label="account of current user"
+              aria-controls={menuId}
+              aria-haspopup="true"
+              onClick={handleProfileMenuOpen}
+              color="inherit"
+            >
+              <Avatar alt="username" src="/static/images/avatar/1.jpg" />
+            </IconButton>
+          </Box>
+        </Toolbar>
+      </AppBar>
+      {renderMenu}
+    </Box>
+  );
+}
---
+++ client/src/context/Theme.tsx
@@ -0,0 +1,57 @@
+import { CssBaseline } from '@mui/material';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { nanoid } from 'nanoid';
+import { createContext, ReactNode, useCallback, useState } from 'react';
+
+const themes = {
+  light: createTheme({
+    palette: {
+      mode: 'light',
+      background: {
+        default: '#E5E5E5',
+      },
+    },
+  }),
+  dark: createTheme({
+    palette: {
+      mode: 'dark',
+      background: {
+        default: '#24242b',
+        paper: '#363740',
+      },
+    },
+  }),
+};
+
+export const ThemeContext = createContext<[keyof typeof themes, () => void]>(
+  null!
+);
+
+type Props = {
+  children: ReactNode;
+};
+
+export function ThemeContextProvider({ children }: Props) {
+  const type = localStorage.getItem('notedly-theme') || 'dark';
+  const [theme, setTheme] = useState(type as keyof typeof themes);
+
+  const toggleMode = useCallback(() => {
+    setTheme((state) => {
+      if (state === 'dark') {
+        localStorage.setItem('notedly-theme', 'light');
+        return 'light';
+      }
+      localStorage.setItem('notedly-theme', 'dark');
+      return 'dark';
+    });
+  }, []);
+
+  return (
+    <ThemeProvider theme={themes[theme]}>
+      <ThemeContext.Provider value={[theme, toggleMode]}>
+        <CssBaseline />
+        {children}
+      </ThemeContext.Provider>
+    </ThemeProvider>
+  );
+}
