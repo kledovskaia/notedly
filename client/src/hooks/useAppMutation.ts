ab0ff5a1e286aef3071695c620eda3034ec496bb
fuxleep <MaddisonMatthewsWebDev@gmail.com>
2022-03-08 17:32:23 +0300

    Added: loading on mutation

--- client/src/hooks/useAppMutation.ts
+++ client/src/hooks/useAppMutation.ts
@@ -1,4 +1,6 @@
 import { OperationVariables, useMutation } from '@apollo/client';
+import { useContext, useEffect } from 'react';
+import { LoadingContext } from '../context/Loading';
 import * as mutations from '../graphql/mutation';
 
 type TMutation = keyof typeof mutations;
@@ -7,5 +9,14 @@ export const useAppMutation = <T>(
   type: TMutation,
   options?: OperationVariables
 ) => {
-  return useMutation<T>(mutations[type], options);
+  const result = useMutation<T>(mutations[type], options);
+
+  const { isLoading, setIsLoading } = useContext(LoadingContext);
+  const [, { loading }] = result;
+
+  useEffect(() => {
+    if (loading !== isLoading) setIsLoading(loading);
+  }, [loading]);
+
+  return result;
 };
